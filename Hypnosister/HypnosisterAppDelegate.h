91f77faedcff2462ca968bceb865aac32e8709ae
Carlo Andaya <carlo@carloandaya.com>
2012-10-08 22:12:35 -0700

    Project created

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Carlo Andaya on 10/8/12.
+//  Copyright (c) 2012 Carlo Andaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
