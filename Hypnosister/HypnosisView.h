c1cf5a4a63d4883c4ada92d16fba13e91aa9780d
Carlo Andaya <carlo@carloandaya.com>
2012-10-09 21:38:10 -0700

    Draw two circles.

---
+++ Hypnosister/HypnosisView.h
@@ -0,0 +1,14 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Carlo Andaya on 10/8/12.
+//  Copyright (c) 2012 Carlo Andaya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HypnosisView : UIView
+
+
+@end
